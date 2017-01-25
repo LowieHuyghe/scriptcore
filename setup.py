0bb779eaa48110c0a33f9c741d4fea00f59383bf
Lowie Huyghe <iam@lowiehuyghe.com>
2017-01-26 00:43:04 +0100

    Read requirements from requirements.txt.

--- setup.py
+++ setup.py
@@ -11,12 +11,23 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import re
+
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+# Get the requires from the requirements.txt
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = []
+    for line in f.readlines():
+        match = re.search("^\s*([^#]+?)\s*[#\n$]", line)
+        if match:
+            requirements.append(match.group(1))
+
 
 setup(
     name='scriptcore',
@@ -79,13 +90,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'asciimatics == 1.7.2',
-        'future==0.16.0',            # via asciimatics
-        'olefile==0.44',             # via pillow
-        'Pillow==4.0.0',             # via asciimatics
-        'pyfiglet==0.7.5',           # via asciimatics
-    ],
+    install_requires=requirements,
 
     setup_requires=[
         'setuptools_scm'
