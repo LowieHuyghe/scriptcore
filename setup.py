44cad2a3f81486cbdbe9354eac6cd254c1f2dc4b
Lowie Huyghe <iam@lowiehuyghe.com>
2017-01-26 01:06:01 +0100

    Moved src to scriptcore to fix problems with submodules.

--- setup.py
+++ setup.py
@@ -81,10 +81,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    package_dir={
-        'scriptcore': 'src'
-    },
-    packages=['scriptcore'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
