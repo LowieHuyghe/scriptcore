afab327d512ef45d5a7c1161f08760a349327f65
Lowie Huyghe <iam@lowiehuyghe.com>
2017-02-07 00:51:51 +0100

    Patched original input functions instead of own input function.

--- tests/console/input/testinput.py
+++ tests/console/input/testinput.py
@@ -6,12 +6,10 @@ import mock
 
 class TestInput(TestCase):
 
-    @mock.patch.object(Input, '_input', return_value='e8n0dpankpdvdni3kgac')
-    def test_default(self, patched_input):
+    def test_default(self):
         """
         Test default input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         description = self.rand_str()
@@ -19,133 +17,125 @@ class TestInput(TestCase):
 
         input = Input()
 
-        value = input(description)
+        with mock.patch(self._get_pathable_input(), return_value='e8n0dpankpdvdni3kgac'):
+            value = input(description)
         self.assert_in(description, self.stdout.getvalue())
         self.assert_equal('e8n0dpankpdvdni3kgac', value)
 
-        value = input.text(description2)
+        with mock.patch(self._get_pathable_input(), return_value='e8n0dpankpdvdni3kgac'):
+            value = input.text(description2)
         self.assert_in(description2, self.stdout.getvalue())
         self.assert_equal('e8n0dpankpdvdni3kgac', value)
 
-    @mock.patch.object(Input, '_input', return_value='')
-    def test_default_empty(self, patched_input):
+    def test_default_empty(self):
         """
         Test default empty input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         input = Input()
-        value = input('')
+        with mock.patch(self._get_pathable_input(), return_value=''):
+            value = input('')
 
         self.assert_is_none(value)
 
-    @mock.patch.object(Input, '_input', return_value='4815162342')
-    def test_integer(self, patched_input):
+    def test_integer(self):
         """
         Test integer input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         description = self.rand_str()
 
         input = Input()
-        value = input.integer(description)
+        with mock.patch(self._get_pathable_input(), return_value='4815162342'):
+            value = input.integer(description)
 
         self.assert_in(description, self.stdout.getvalue())
         self.assert_equal(4815162342, value)
 
-    @mock.patch.object(Input, '_input', return_value='Ygritte dies')
-    def test_integer_invalid(self, patched_input):
+    def test_integer_invalid(self):
         """
         Test integer invalid input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         input = Input()
-        value = input.integer('')
+        with mock.patch(self._get_pathable_input(), return_value='Ygritte dies'):
+            value = input.integer('')
 
         self.assert_is_none(value)
 
-    @mock.patch.object(Input, '_input', return_value='4.2')
-    def test_float(self, patched_input):
+    def test_float(self):
         """
         Test float input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         description = self.rand_str()
 
         input = Input()
-        value = input.float(description)
+        with mock.patch(self._get_pathable_input(), return_value='4.2'):
+            value = input.float(description)
 
         self.assert_in(description, self.stdout.getvalue())
         self.assert_equal(4.2, value)
 
-    @mock.patch.object(Input, '_input', return_value='Peanut butter jelly time')
-    def test_float_invalid(self, patched_input):
+    def test_float_invalid(self):
         """
         Test float invalid input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         input = Input()
-        value = input.float('')
+        with mock.patch(self._get_pathable_input(), return_value='Peanut butter jelly time'):
+            value = input.float('')
 
         self.assert_is_none(value)
 
-    @mock.patch.object(Input, '_input', return_value='y')
-    def test_yes_no(self, patched_input):
+    def test_yes_no(self):
         """
         Test yes_no input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         description = self.rand_str()
 
         input = Input()
-        value = input.yes_no(description)
+        with mock.patch(self._get_pathable_input(), return_value='y'):
+            value = input.yes_no(description)
 
         self.assert_in(description, self.stdout.getvalue())
         self.assert_true(value)
 
-    @mock.patch.object(Input, '_input', return_value='n')
-    def test_yes_no_no(self, patched_input):
+    def test_yes_no_no(self):
         """
         Test yes_no no input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         input = Input()
-        value = input.yes_no('')
+        with mock.patch(self._get_pathable_input(), return_value='n'):
+            value = input.yes_no('')
 
         self.assert_false(value)
 
-    @mock.patch.object(Input, '_input', return_value='Indiana Jones dies in Star Wars')
-    def test_yes_no_invalid(self, patched_input):
+    def test_yes_no_invalid(self):
         """
         Test yes_no invalid input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         input = Input()
-        value = input.yes_no('')
+        with mock.patch(self._get_pathable_input(), return_value='Indiana Jones dies in Star Wars'):
+            value = input.yes_no('')
 
         self.assert_false(value)
 
-    @mock.patch.object(Input, '_input', return_value='2')
-    def test_pick(self, patched_input):
+    def test_pick(self):
         """
         Test pick input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         description = self.rand_str()
@@ -157,17 +147,16 @@ class TestInput(TestCase):
         ]
 
         input = Input()
-        value = input.pick(options, description)
+        with mock.patch(self._get_pathable_input(), return_value='2'):
+            value = input.pick(options, description)
 
         self.assert_in(description, self.stdout.getvalue())
         self.assert_equal(2, value)
 
-    @mock.patch.object(Input, '_input', return_value='6')
-    def test_pick_out_of_range(self, patched_input):
+    def test_pick_out_of_range(self):
         """
         Test pick out of range input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         description = self.rand_str()
@@ -179,17 +168,16 @@ class TestInput(TestCase):
         ]
 
         input = Input()
-        value = input.pick(options, description)
+        with mock.patch(self._get_pathable_input(), return_value='6'):
+            value = input.pick(options, description)
 
         self.assert_in(description, self.stdout.getvalue())
         self.assert_is_none(value)
 
-    @mock.patch.object(Input, '_input', return_value='Scoobiedoobiedoo')
-    def test_pick_invalid(self, patched_input):
+    def test_pick_invalid(self):
         """
         Test pick invalid input
-        :param patched_input:   The patched input
-        :return:                void
+        :return:    void
         """
 
         description = self.rand_str()
@@ -201,7 +189,20 @@ class TestInput(TestCase):
         ]
 
         input = Input()
-        value = input.pick(options, description)
+        with mock.patch(self._get_pathable_input(), return_value='Scoobiedoobiedoo'):
+            value = input.pick(options, description)
 
         self.assert_in(description, self.stdout.getvalue())
         self.assert_is_none(value)
+
+    def _get_pathable_input(self):
+        """
+        Get the patchable input
+        :return:    Patchable input
+        """
+
+        try:
+            raw_input
+            return '__builtin__.raw_input'
+        except NameError:
+            return 'builtins.input'
