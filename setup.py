9334f98e02a4ac9adcd6d2a23e81a3ecc5f86169
Lowie Huyghe <iam@lowiehuyghe.com>
2017-02-08 23:50:56 +0100

    Made a simpler setup.py with a config-file.

--- setup.py
+++ setup.py
@@ -1,98 +1,230 @@
-"""
-A setuptools based setup module for scriptcore.
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-"""
-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+import os.path
+import pprint
 import re
+import setuptools
+import sys
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
 
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# Get the requires from the requirements.txt
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    requirements = []
-    for line in f.readlines():
-        match = re.search("^\s*([^#]+?)\s*[#\n$]", line)
-        if match:
-            requirements.append(match.group(1))
+# The path to the classifiers and the config
+classifiers_path = 'setup.classifiers.txt'
+config_path = 'setup.config.ini'
 
 
-setup(
-    name='scriptcore',
+# Setup-object
+class Setup(object):
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    def __init__(self, base_path, classifiers_path, config_path):
+        """
+        Construct setup
+        :param base_path:           The base path
+        :param classifiers_path:    Path to the classifiers
+        :param config_path:         Path to the config
+        """
 
-    description='A core to start from when writing scripts.',
-    long_description=long_description,
+        self.base_path = base_path
 
-    # The project's main homepage.
-    url='https://github.com/LowieHuyghe/script-core',
+        self.classifiers = self._load_classifiers(classifiers_path)
+        self.config = self._load_config(config_path)
 
-    # Author details
-    author='Lowie Huyghe',
-    author_email='iam@lowiehuyghe.com',
+    def _load_classifiers(self, classifiers_path):
+        """
+        Load classifiers
+        :param classifiers_path:    Path to the classifiers
+        :return:                    list
+        """
 
-    # Choose your license
-    license='Apache-2.0',
+        classifiers = []
+        with open(os.path.join(self.base_path, classifiers_path)) as classifiers_file:
+            for classifier_line in classifiers_file:
+                classifier_match = re.search('^([^#]*)', classifier_line)
+                if not classifier_match:
+                    continue
+                classifier = classifier_match.group(1).strip()
+                if not classifier:
+                    continue
+                classifiers.append(classifier)
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        return classifiers
 
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+    def _load_config(self, config_path):
+        """
+        Load the config
+        :param config_path: Path to the config
+        :return:            Config-dict
+        """
 
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache License 2.0',
+        config_parser = ConfigParser.ConfigParser()
+        config_parser.read(os.path.join(self.base_path, config_path))
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+        config = {}
+        for section in config_parser.sections():
+            config[section.lower()] = {}
+            for option in config_parser.options(section):
+                config[section.lower()][option.lower()] = config_parser.get(section, option)
 
-    # What does your project relate to?
-    keywords='script core development',
+        return config
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    def _process_argument(self, arguments, argument_key, config_section, config_key, value_type):
+        """
+        Process argument
+        :param arguments:       The dict of arguments to add to
+        :param argument_key:    The key to add to the dict
+        :param config_section:  The config section
+        :param config_key:      The config key
+        :param value_type:      The value type
+        :return:                void
+        """
 
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=requirements,
+        value = self._process_value(config_section, config_key, value_type)
+        if value is None:
+            return
 
-    setup_requires=[
-        'setuptools_scm'
-    ],
+        arguments[argument_key] = value
 
-    test_suite='nose.collector',
-    tests_require=['nose']
-)
+    def _process_classifier(self, classifiers, config_section, config_key, value_type, regex):
+        """
+        Process classifier
+        :param classifiers:     The list of classifiers to add to
+        :param config_section:  The config section
+        :param config_key:      The config key
+        :param value_type:      The value type
+        :param regex:           The regex to match
+        :return:                void
+        """
+
+        value = self._process_value(config_section, config_key, value_type)
+        if value is None:
+            return
+
+        if not isinstance(value, list):
+            value = [value]
+
+        for item in value:
+            matched_classifiers = []
+            for classifier in self.classifiers:
+                if re.match(regex.replace('{0}', item.replace(' ', '[\s:]*?')), classifier):
+                    matched_classifiers.append(classifier)
+
+            if not matched_classifiers:
+                self.exit('No classifiers found for: \'%s\' > \'%s\' > \'%s\'' % (config_section, config_key, item))
+            elif len(matched_classifiers) > 1:
+                self.exit('Multiple classifiers found for: \'%s\' > \'%s\' > \'%s\'' % (config_section, config_key, item))
+            else:
+                classifiers.extend(matched_classifiers)
+
+    def _process_value(self, config_section, config_key, value_type):
+        """
+        Process the value
+        :param config_section:  The section in the config
+        :param config_key:      The key in the config
+        :param value_type:      The type of the value
+        :return:                The processed value
+        """
+
+        if config_section not in self.config:
+            return None
+        if config_key not in self.config[config_section]:
+            return None
+
+        value = self.config[config_section][config_key]
+
+        if value_type == str:
+            if value.startswith('file://'):
+                value_path = os.path.join(self.base_path, value[len('file://'):])
+                with open(value_path, 'r') as value_file:
+                    value = value_file.read()
+        elif value_type == list:
+            if value.startswith('file://'):
+                value_path = os.path.join(self.base_path, value[len('file://'):])
+                with open(value_path, 'r') as value_file:
+                    value = value_file.readlines()
+                    value = filter(lambda k: bool(k), value)
+                    value = map(lambda k: k.strip().replace('\n', ''), value)
+            else:
+                value = value.split(',')
+
+        return value
+
+    def _load_arguments(self):
+        """
+        Get the setup arguments
+        :return:    The setup arguments
+        """
+
+        setup_arguments = dict()
+
+        # General
+        self._process_argument(setup_arguments, 'name', 'general', 'name', str)
+        self._process_argument(setup_arguments, 'version', 'general', 'version', str)
+        self._process_argument(setup_arguments, 'description', 'general', 'description', str)
+        self._process_argument(setup_arguments, 'long_description', 'general', 'long_description', str)
+        self._process_argument(setup_arguments, 'url', 'general', 'url', str)
+        self._process_argument(setup_arguments, 'license', 'general', 'license', str)
+        self._process_argument(setup_arguments, 'install_requires', 'general', 'requirements', list)
+
+        # Author
+        self._process_argument(setup_arguments, 'author', 'author', 'name', str)
+        self._process_argument(setup_arguments, 'author_email', 'author', 'email', str)
+
+        # Classifiers
+        classifiers = []
+        self._process_classifier(classifiers, 'classifiers', 'status', list, '^Development Status :: ({0} - \w+|\d+ - {0})')
+        self._process_classifier(classifiers, 'classifiers', 'programming_languages', list, '^Programming Language :: {0}$')
+        self._process_classifier(classifiers, 'classifiers', 'audiences', list, '^Intended Audience :: .*?{0}.*?')
+        self._process_classifier(classifiers, 'classifiers', 'topics', list, '^Topic :: .*?{0}.*?')
+        self._process_classifier(classifiers, 'classifiers', 'license', list, '^License :: .*?{0}.*?')
+        if classifiers:
+            setup_arguments['classifiers'] = list(set(classifiers))
+
+        # Setup
+        self._process_argument(setup_arguments, 'setup_requires', 'setup', 'requirements', list)
+
+        # Tests
+        self._process_argument(setup_arguments, 'tests_require', 'tests', 'requirements', list)
+        self._process_argument(setup_arguments, 'test_suite', 'tests', 'suite', str)
+
+        # Packages
+        packages_arguments = dict()
+        self._process_argument(packages_arguments, 'exclude', 'packages', 'exclude', list)
+        if packages_arguments:
+            setup_arguments['packages'] = setuptools.find_packages(**packages_arguments)
+
+        return setup_arguments
+
+    def exit(self, reason, exitcode=1):
+        """
+        Exit the program
+        :param reason:      The reason
+        :param exitcode:    The exitcode
+        :return:            void
+        """
+
+        if reason:
+            print('\033[0;31m%s\033[0m' % reason)
+        sys.exit(exitcode)
+
+    def setup(self):
+        """
+        Setup
+        :return:    void
+        """
+
+        arguments = self._load_arguments()
+
+        if '--debug' in sys.argv:
+            print('Loading setup with the following kwargs:')
+            pp = pprint.PrettyPrinter(indent=4, depth=4)
+            pp.pprint(arguments)
+
+        else:
+            setuptools.setup(**arguments)
+
+
+# Construct and setup
+base_path = os.path.abspath(os.path.dirname(__file__))
+Setup(base_path, classifiers_path, config_path).setup()
